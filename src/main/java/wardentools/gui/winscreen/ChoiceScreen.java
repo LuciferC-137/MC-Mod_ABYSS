323a389976a0489f3d530238436a38817fcec8a8
Maxime Bloch <maxoscar.bloch@gmail.com>
2024-11-02 00:11:12 +0100

    Choice Screen renderer
    Added new visuals for the choice screen. No logic implemented yet.

--- src/main/java/wardentools/gui/winscreen/ChoiceScreen.java
+++ src/main/java/wardentools/gui/winscreen/ChoiceScreen.java
@@ -2,7 +2,6 @@ package wardentools.gui.winscreen;
 
 import com.mojang.blaze3d.platform.GlStateManager;
 import com.mojang.blaze3d.systems.RenderSystem;
-import com.mojang.logging.LogUtils;
 import net.minecraft.client.GameNarrator;
 import net.minecraft.client.gui.GuiGraphics;
 import net.minecraft.client.gui.components.Button;
@@ -13,41 +12,70 @@ import net.minecraft.sounds.Music;
 import net.minecraftforge.api.distmarker.Dist;
 import net.minecraftforge.api.distmarker.OnlyIn;
 import org.jetbrains.annotations.NotNull;
-import org.slf4j.Logger;
 import wardentools.ModMain;
 import wardentools.sounds.ModMusics;
+
+import java.util.Random;
 
 @OnlyIn(Dist.CLIENT)
 public class ChoiceScreen extends Screen {
    private static final ResourceLocation VIGNETTE_LOCATION
            = new ResourceLocation("textures/misc/vignette.png");
-   private static final ResourceLocation MOD_BACKGROUND_LOCATION
-           = new ResourceLocation(ModMain.MOD_ID, "textures/gui/background.png");
+   private static final ResourceLocation CORRUPTION_LOCATION
+              = new ResourceLocation(ModMain.MOD_ID, "textures/gui/choice_screen/corruption.png");
+   private static final ResourceLocation RADIANCE_LOCATION
+              = new ResourceLocation(ModMain.MOD_ID, "textures/gui/choice_screen/radiance.png");
+   private static final ResourceLocation CORRUPTION_BUTTON_OVERLAY
+              = new ResourceLocation(ModMain.MOD_ID, "textures/gui/choice_screen/corruption_button_overlay.png");
+   private static final ResourceLocation RADIANCE_BUTTON_OVERLAY
+              = new ResourceLocation(ModMain.MOD_ID, "textures/gui/choice_screen/radiance_button_overlay.png");
+   private static final int BUTTON_WIDTH = 100;
+   private static final int BUTTON_HEIGHT = 20;
+   private static final int OVERLAY_MARGIN = 40;
    private final Runnable onFinished;
+   private Button contagionButton;
+   private Button radianceButton;
+   private float spreadFactor = 0.5f;
+   private static final float MAX_CENTER_FACTOR = 0.7f;
+   private static final float MIN_CENTER_FACTOR = 0.3f;
+   private static final float SPREAD_FACTOR = 0.02f;
+   private long seed = 123456789L;
 
-    public ChoiceScreen(Runnable runnable) {
+   public ChoiceScreen(Runnable runnable) {
       super(GameNarrator.NO_TITLE);
       this.onFinished = runnable;
    }
 
    @Override
    protected void init() {
-      int buttonWidth = 200;
-      int buttonHeight = 20;
-      int x = (this.width - buttonWidth) / 2;
-      int y = this.height / 2 - 24;
-      Button radianceButton = Button.builder(Component.literal("Radiance"),
-              (button) -> this.respawn()).bounds(x, y, buttonWidth, buttonHeight).build();
-      Button contagionButton = Button.builder(Component.literal("Contagion"),
-              (button) -> this.respawn()).bounds(x, y + 24, buttonWidth, buttonHeight).build();
-      this.addRenderableWidget(radianceButton);
-      this.addRenderableWidget(contagionButton);
+      int spacing = 30;
+      int x1 = (this.width / 2) - BUTTON_WIDTH - spacing;
+      int x2 = (this.width / 2) + spacing;
+      int y = this.height / 2 - 10;
+      this.contagionButton = Button.builder(Component.literal("Contagion"),
+              (button) -> this.respawn()).bounds(x1, y, BUTTON_WIDTH, BUTTON_HEIGHT).build();
+      this.radianceButton = Button.builder(Component.literal("Radiance"),
+              (button) -> this.respawn()).bounds(x2, y, BUTTON_WIDTH, BUTTON_HEIGHT).build();
+      this.addRenderableWidget(this.radianceButton);
+      this.addRenderableWidget(this.contagionButton);
+      this.seed = new Random().nextLong();
    }
 
    public void tick() {
       if (this.minecraft != null) {
          this.minecraft.getMusicManager().tick();
          this.minecraft.getSoundManager().tick(false);
+         if (!this.minecraft.getMusicManager().isPlayingMusic(ModMusics.DEEP_FOREST)) {
+            this.minecraft.getMusicManager().startPlaying(ModMusics.DEEP_FOREST);
+         }
+      }
+      if (this.radianceButton.isHovered() && this.spreadFactor < MAX_CENTER_FACTOR) {
+         this.spreadFactor += SPREAD_FACTOR;
+      } else if (this.contagionButton.isHovered() && this.spreadFactor > MIN_CENTER_FACTOR) {
+         this.spreadFactor -= SPREAD_FACTOR;
+      } else if (!this.radianceButton.isHovered() && !this.contagionButton.isHovered()) {
+         if (this.spreadFactor > 0.5f + SPREAD_FACTOR / 2f) this.spreadFactor -= SPREAD_FACTOR;
+         else if (this.spreadFactor < 0.5f - SPREAD_FACTOR / 2f) this.spreadFactor += SPREAD_FACTOR;
       }
    }
 
@@ -57,23 +85,66 @@ public class ChoiceScreen extends Screen {
 
    public void render(@NotNull GuiGraphics graphics, int x, int y, float a) {
       super.render(graphics, x, y, a);
-      int i = this.width / 2 - 128;
-      int j = this.height + 50;
       RenderSystem.enableBlend();
-      RenderSystem.blendFunc(GlStateManager.SourceFactor.ZERO,
-              GlStateManager.DestFactor.ONE_MINUS_SRC_COLOR);
+      RenderSystem.blendFunc(GlStateManager.SourceFactor.ZERO, GlStateManager.DestFactor.ONE_MINUS_SRC_COLOR);
       graphics.blit(VIGNETTE_LOCATION, 0, 0,
               0, 0.0F, 0.0F, this.width, this.height, this.width, this.height);
       RenderSystem.disableBlend();
       RenderSystem.defaultBlendFunc();
+      this.renderButtonOverlay(graphics);
+   }
+
+   private void renderButtonOverlay(@NotNull GuiGraphics graphics) {
+      graphics.blit(CORRUPTION_BUTTON_OVERLAY,
+              this.contagionButton.getX() - OVERLAY_MARGIN / 2,
+              this.contagionButton.getY() - OVERLAY_MARGIN / 2,
+              0, 0.0F, 0.0F,
+              this.contagionButton.getWidth() + OVERLAY_MARGIN,
+              this.contagionButton.getHeight()  + OVERLAY_MARGIN,
+              this.contagionButton.getWidth()  + OVERLAY_MARGIN,
+              this.contagionButton.getHeight()  + OVERLAY_MARGIN);
+      graphics.blit(RADIANCE_BUTTON_OVERLAY,
+              this.radianceButton.getX() - OVERLAY_MARGIN / 2,
+              this.radianceButton.getY()  - OVERLAY_MARGIN / 2,
+              0, 0.0F, 0.0F,
+              this.radianceButton.getWidth() + OVERLAY_MARGIN,
+              this.radianceButton.getHeight()  + OVERLAY_MARGIN,
+              this.radianceButton.getWidth()  + OVERLAY_MARGIN,
+              this.radianceButton.getHeight()  + OVERLAY_MARGIN);
    }
 
    public void renderBackground(@NotNull GuiGraphics graphics, int x, int y, float a) {
-      int i = this.width;
-      int j = 64;
-      graphics.blit(MOD_BACKGROUND_LOCATION, 0, 0, 0, 0.0F,
-              0, i, this.height, j, j);
-      graphics.setColor(1.0F, 1.0F, 1.0F, 1.0F);
+      Random random = new Random(this.seed);
+      graphics.setColor(0.5F, 0.5F, 0.5F, 1F);
+      float min = 0.1f * this.width;
+      float max = 0.8f * this.width;
+      for (int i = 0; i < (this.width / 16) + 1; i++) {
+         for (int j = 0; j < (this.height / 16) + 1; j++) {
+            if (i*16 < min) {
+               graphics.blit(CORRUPTION_LOCATION, i * 16, j * 16,
+                       0, 0.0F, 0,
+                       16, 16, 16, 16);
+            } else if (i*16 > max) {
+               graphics.blit(RADIANCE_LOCATION, i * 16, j * 16,
+                       0, 0.0F, 0,
+                       16, 16, 16, 16);
+            } else {
+               float distanceFromLeft = (float)i * 16 - min;
+               float maxDistance = max - min;
+               float contagionProbability = Math.max(0.0F,
+                       1.0F - distanceFromLeft / maxDistance * (2f * this.spreadFactor));
+               if (random.nextFloat() < contagionProbability) {
+                  graphics.blit(CORRUPTION_LOCATION, i * 16, j * 16,
+                          0, 0.0F, 0,
+                          16, 16, 16, 16);
+               } else {
+                  graphics.blit(RADIANCE_LOCATION, i * 16, j * 16,
+                          0, 0.0F, 0,
+                          16, 16, 16, 16);
+               }
+            }
+         }
+      }
    }
 
    public void removed() {
