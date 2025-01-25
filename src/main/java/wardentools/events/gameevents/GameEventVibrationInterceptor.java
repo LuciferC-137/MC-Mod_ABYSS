856ee58e1dade2125644746cca1a638a2f30a23d
Maxime Bloch <maxoscar.bloch@gmail.com>
2025-01-25 18:40:02 +0100

    Fixed stealth enchantment

--- src/main/java/wardentools/events/gameevents/GameEventVibrationInterceptor.java
+++ src/main/java/wardentools/events/gameevents/GameEventVibrationInterceptor.java
@@ -1,15 +1,18 @@
 package wardentools.events.gameevents;
 
+import net.minecraft.core.Holder;
+import net.minecraft.core.Registry;
+import net.minecraft.core.RegistryAccess;
 import net.minecraft.core.registries.Registries;
 import net.minecraft.world.entity.EquipmentSlot;
 import net.minecraft.world.entity.player.Player;
 import net.minecraft.world.item.ItemStack;
+import net.minecraft.world.item.enchantment.Enchantment;
 import net.minecraft.world.item.enchantment.EnchantmentHelper;
 import net.minecraft.world.level.gameevent.GameEvent;
 import net.minecraftforge.event.VanillaGameEvent;
 import net.minecraftforge.eventbus.api.SubscribeEvent;
 import net.minecraftforge.fml.common.Mod;
-import net.minecraftforge.registries.ForgeRegistries;
 import wardentools.ModMain;
 import wardentools.items.enchantment.EnchantmentRegistry;
 
@@ -19,41 +22,39 @@ public class GameEventVibrationInterceptor {
     @SubscribeEvent
     public static void onGameEvent(VanillaGameEvent event) {
         if (event.getCause() instanceof Player player) {
-            if (EnchantmentRegistry.STEALTH.getHolder().isEmpty()) {
-                System.out.println(EnchantmentRegistry.STEALTH);
-                System.out.println("Stealth enchantment not found!");
-                return;
-            }
             ItemStack helmet = player.getItemBySlot(EquipmentSlot.HEAD);
             ItemStack chestplate = player.getItemBySlot(EquipmentSlot.CHEST);
             ItemStack leggings = player.getItemBySlot(EquipmentSlot.LEGS);
             ItemStack boots = player.getItemBySlot(EquipmentSlot.FEET);
-            if (isStealthEnchanted(boots) && isStealthEnchanted(helmet)
-                    && isStealthEnchanted(leggings) && isStealthEnchanted(chestplate)) {
+            if (isStealthEnchanted(player, boots) && isStealthEnchanted(player, helmet)
+                    && isStealthEnchanted(player, leggings) && isStealthEnchanted(player, chestplate)) {
                 event.setCanceled(true);
                 return;
             }
             if (event.getVanillaEvent() == GameEvent.HIT_GROUND.get()) {
-                if (isStealthEnchanted(leggings)) {
+                if (isStealthEnchanted(player, leggings)) {
                     event.setCanceled(true);
                 }
             } else if (event.getVanillaEvent() == GameEvent.BLOCK_PLACE.get()
                     || event.getVanillaEvent() == GameEvent.BLOCK_DESTROY.get()
                     || event.getVanillaEvent() == GameEvent.BLOCK_CHANGE.get()) {
-                if (isStealthEnchanted(chestplate)) {
+                if (isStealthEnchanted(player, chestplate)) {
                     event.setCanceled(true);
                 }
             } else if (event.getVanillaEvent() == GameEvent.STEP.get()) {
-                if (isStealthEnchanted(boots)) {
+                if (isStealthEnchanted(player, boots)) {
                     event.setCanceled(true);
                 }
             }
         }
     }
 
-    private static boolean isStealthEnchanted(ItemStack stack) {
-        return EnchantmentHelper.getItemEnchantmentLevel(
-                EnchantmentRegistry.STEALTH.getHolder().get(), stack) > 0;
+    private static boolean isStealthEnchanted(Player player, ItemStack stack) {
+        if (stack.isEmpty()) return false;
+        RegistryAccess registryAccess = player.registryAccess();
+        Registry<Enchantment> enchantmentRegistry = registryAccess.registryOrThrow(Registries.ENCHANTMENT);
+        Holder<Enchantment> stealthEnchantment = enchantmentRegistry.getHolderOrThrow(EnchantmentRegistry.STEALTH);
+        return EnchantmentHelper.getItemEnchantmentLevel(stealthEnchantment, stack) > 0;
     }
 
 }
--- src/main/java/wardentools/items/enchantment/EnchantmentRegistry.java
+++ src/main/java/wardentools/items/enchantment/EnchantmentRegistry.java
@@ -1,18 +1,16 @@
 package wardentools.items.enchantment;
 
 import net.minecraft.core.registries.Registries;
+import net.minecraft.resources.ResourceKey;
 import net.minecraft.resources.ResourceLocation;
 import net.minecraft.world.item.enchantment.Enchantment;
-import net.minecraftforge.registries.RegistryObject;
 import wardentools.ModMain;
 
 public class EnchantmentRegistry {
-    public static final RegistryObject<Enchantment> STEALTH = register("stealth");
+    public static final ResourceKey<Enchantment> STEALTH = register("stealth");
     
-    public static RegistryObject<Enchantment> register(String name) {
-        return RegistryObject.create(
-                ResourceLocation.fromNamespaceAndPath(ModMain.MOD_ID, name),
-                Registries.ENCHANTMENT.location(), ModMain.MOD_ID);
+    public static ResourceKey<Enchantment> register(String name) {
+        return ResourceKey.create(Registries.ENCHANTMENT,
+                ResourceLocation.fromNamespaceAndPath(ModMain.MOD_ID, name));
     }
-
 }
