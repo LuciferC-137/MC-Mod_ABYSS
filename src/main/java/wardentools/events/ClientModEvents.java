f537e59e1c2799a978e9a25502c4eec762c4b266
Maxime Bloch <maxoscar.bloch@gmail.com>
2025-02-07 09:23:13 +0100

    Fixed cutout and translucent textures

--- src/main/java/wardentools/events/ClientModEvents.java
+++ src/main/java/wardentools/events/ClientModEvents.java
@@ -1,12 +1,17 @@
 package wardentools.events;
 
 import net.minecraft.client.model.BoatModel;
+import net.minecraft.client.renderer.ItemBlockRenderTypes;
+import net.minecraft.client.renderer.RenderType;
+import net.minecraft.world.level.block.Block;
 import net.minecraftforge.api.distmarker.Dist;
 import net.minecraftforge.client.event.EntityRenderersEvent;
 import net.minecraftforge.eventbus.api.SubscribeEvent;
 import net.minecraftforge.fml.common.Mod;
 import net.minecraftforge.fml.event.lifecycle.FMLClientSetupEvent;
+import net.minecraftforge.registries.RegistryObject;
 import wardentools.ModMain;
+import wardentools.block.BlockRegistry;
 import wardentools.entity.ModEntities;
 import wardentools.entity.client.model.*;
 import wardentools.entity.client.renderer.*;
@@ -24,6 +29,7 @@ public class ClientModEvents {
 	@SubscribeEvent
 	public static void clientSetup(FMLClientSetupEvent event) {
 		event.enqueueWork(ModItemProperties::addCustomProperties);
+		event.enqueueWork(ClientModEvents::registerBlockRenderTypes);
 	}
 
 	@SubscribeEvent
@@ -66,5 +72,48 @@ public class ClientModEvents {
 		event.registerLayerDefinition(Shadow.LAYER_LOCATION, Shadow::createBodyLayer);
 		event.registerLayerDefinition(ContagionIncarnationCorpse.LAYER_LOCATION,
 				ContagionIncarnationCorpse::createBodyLayer);
+	}
+
+	public static void registerBlockRenderTypes() {
+		cutout(BlockRegistry.SOUL_SPAWNER);
+		cutout(BlockRegistry.PROTECTOR_INVOKER);
+		cutout(BlockRegistry.RADIANCE_CATALYST);
+		cutout(BlockRegistry.WIND_WHISPERER);
+		cutout(BlockRegistry.DARKTREE_LEAVES);
+		cutout(BlockRegistry.WHITETREE_LEAVES);
+		cutout(BlockRegistry.DARKTREE_SAPLING);
+		cutout(BlockRegistry.WHITETREE_SAPLING);
+		cutout(BlockRegistry.DARK_GRASS);
+		cutout(BlockRegistry.TALL_DARK_GRASS);
+		cutout(BlockRegistry.WHITE_GRASS);
+		cutout(BlockRegistry.TALL_WHITE_GRASS);
+		cutout(BlockRegistry.WHITE_TORCHFLOWER);
+		cutout(BlockRegistry.DEEPFLOWER);
+		cutout(BlockRegistry.BLUE_BUSH);
+		cutout(BlockRegistry.POTTED_WHITE_TORCHFLOWER);
+		cutout(BlockRegistry.DARKTREE_DOOR);
+		cutout(BlockRegistry.WHITETREE_DOOR);
+		cutout(BlockRegistry.DARKTREE_TRAPDOOR);
+		cutout(BlockRegistry.WHITETREE_TRAPDOOR);
+		cutout(BlockRegistry.PALE_CRISTAL);
+		cutout(BlockRegistry.ECHO_CRISTAL);
+		cutout(BlockRegistry.CITRINE);
+		cutout(BlockRegistry.MALACHITE);
+		cutout(BlockRegistry.RUBY);
+		cutout(BlockRegistry.RADIANCE_CRISTAL);
+		cutout(BlockRegistry.DEEP_CRISTAL);
+
+		translucent(BlockRegistry.DYSFUNCTIONNING_CATALYST);
+		translucent(BlockRegistry.REINFORCED_GLASS);
+	}
+
+	@SuppressWarnings("deprecation")
+	public static void cutout(RegistryObject<? extends Block> block) {
+		ItemBlockRenderTypes.setRenderLayer(block.get(), RenderType.cutout());
+	}
+
+	@SuppressWarnings("deprecation")
+	public static void translucent(RegistryObject<? extends Block> block) {
+		ItemBlockRenderTypes.setRenderLayer(block.get(), RenderType.translucent());
 	}
 }
