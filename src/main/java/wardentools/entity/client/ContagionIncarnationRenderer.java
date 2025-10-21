1324f4855549a1e7b4215feb27693ea3236505b6
Maxime Bloch <maxoscar.bloch@gmail.com>
2025-10-21 12:02:38 +0200

    Improved Contagion Incarnation texture

--- src/main/java/wardentools/entity/client/ContagionIncarnationRenderer.java
+++ src/main/java/wardentools/entity/client/ContagionIncarnationRenderer.java
@@ -23,7 +23,8 @@ import wardentools.particle.ParticleRegistry;
 
 public class ContagionIncarnationRenderer extends MobRenderer<ContagionIncarnationEntity, ContagionIncarnation>{
 	private static final ResourceLocation CONTAGION_INCARNATION_TEXTURE = 
-			ResourceLocation.fromNamespaceAndPath(ModMain.MOD_ID, "textures/entity/contagion_incarnation.png");
+			ResourceLocation.fromNamespaceAndPath(ModMain.MOD_ID,
+					"textures/entity/contagion_incarnation/contagion_incarnation.png");
 	private static final float HALF_SQRT_3 = (float)(Math.sqrt(3.0D) / 2.0D);
 	private static final float PARTICLE_DEATH_RADIUS = 2f;
 	private static final float PARTICLE_SPAWN_RADIUS = 4f;
@@ -33,6 +34,7 @@ public class ContagionIncarnationRenderer extends MobRenderer<ContagionIncarnati
 
 	public ContagionIncarnationRenderer(EntityRendererProvider.Context context) {
 		super(context, new ContagionIncarnation(context.bakeLayer(ContagionIncarnation.LAYER_LOCATION)), 3f);
+		this.addLayer(new ContagionIncarnationShellLayer(this));
 		this.addLayer(new ContagionIncarnationEmissiveLayer(this));
 	}
 
--- src/main/java/wardentools/entity/client/ContagionIncarnationShellLayer.java
+++ src/main/java/wardentools/entity/client/ContagionIncarnationShellLayer.java
@@ -2,7 +2,6 @@ package wardentools.entity.client;
 
 import com.mojang.blaze3d.vertex.PoseStack;
 import com.mojang.blaze3d.vertex.VertexConsumer;
-import net.minecraft.client.renderer.LightTexture;
 import net.minecraft.client.renderer.MultiBufferSource;
 import net.minecraft.client.renderer.RenderType;
 import net.minecraft.client.renderer.entity.LivingEntityRenderer;
@@ -13,12 +12,12 @@ import org.jetbrains.annotations.NotNull;
 import wardentools.ModMain;
 import wardentools.entity.custom.ContagionIncarnationEntity;
 
-public class ContagionIncarnationEmissiveLayer extends RenderLayer<ContagionIncarnationEntity, ContagionIncarnation> {
-    private static final RenderType SHELL = RenderType.entityTranslucent(
+public class ContagionIncarnationShellLayer extends RenderLayer<ContagionIncarnationEntity, ContagionIncarnation> {
+    private static final RenderType EMISSIVE = RenderType.entityTranslucent(
             ResourceLocation.fromNamespaceAndPath(ModMain.MOD_ID,
-    		"textures/entity/contagion_incarnation/contagion_incarnation_emissive.png"));
+    		"textures/entity/contagion_incarnation/contagion_incarnation_shell.png"));
 
-    public ContagionIncarnationEmissiveLayer(RenderLayerParent<ContagionIncarnationEntity, ContagionIncarnation> renderLayerParent) {
+    public ContagionIncarnationShellLayer(RenderLayerParent<ContagionIncarnationEntity, ContagionIncarnation> renderLayerParent) {
         super(renderLayerParent);
     }
 
@@ -27,8 +26,8 @@ public class ContagionIncarnationEmissiveLayer extends RenderLayer<ContagionInca
                        int packedLight, @NotNull ContagionIncarnationEntity entity, float limbSwing,
                        float limbSwingAmount, float partialTicks, float ageInTicks,
                        float netHeadYaw, float headPitch) {
-        VertexConsumer vertexConsumer = bufferSource.getBuffer(SHELL);
-        this.getParentModel().renderToBuffer(poseStack, vertexConsumer, LightTexture.FULL_BRIGHT,
+        VertexConsumer vertexConsumer = bufferSource.getBuffer(EMISSIVE);
+        this.getParentModel().renderToBuffer(poseStack, vertexConsumer, packedLight,
         		LivingEntityRenderer.getOverlayCoords(entity, 0.0F));
     }
 }
