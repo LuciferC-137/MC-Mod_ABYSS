439e9974074f537b63f78ab8a46d93f17962ce25
Maxime Bloch <maxoscar.bloch@gmail.com>
2025-12-02 11:43:30 +0100

    1.1.3

--- src/main/java/wardentools/ModMain.java
+++ src/main/java/wardentools/ModMain.java
@@ -34,7 +34,7 @@ import wardentools.worldgen.tree.ModTrunkPlacerTypes;
 public class ModMain {
 	public static final String MOD_ID = "wardentools";
 	public static final String MODNAME = "ABYSS";
-    public static final String VERSION = "1.1.2";
+    public static final String VERSION = "1.1.3";
     private static final Logger LOGGER = LogUtils.getLogger();
 
 	public ModMain(IEventBus bus, ModContainer container) {
